7942951f09a875ab3c44c6de7f33e3c446843609
J-Pilla <pilla.jason@gmail.com>
2025-11-29 21:52:33 -0500

    made field coinText private

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -2,21 +2,22 @@ using UnityEngine;
 using TMPro;
 public class Coin : MonoBehaviour
 {
-
-    public TextMeshProUGUI coinText;
     [SerializeField] private int coinsToGive = 1;
+
+    private TextMeshProUGUI coinText;
 
     private void Start()
     {
         coinText = GameObject.FindGameObjectWithTag("CoinText").GetComponent<TextMeshProUGUI>();
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             PlayerController player = collision.gameObject.GetComponent<PlayerController>();
-            player.coins += coinsToGive;
-            coinText.text = player.coins.ToString();
+            player.Coins += coinsToGive;
+            coinText.text = player.Coins.ToString();
             Destroy(gameObject);
         }
     }
