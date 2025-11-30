16213c8d2a0c6c99946ef34b71d2b705961b2659
J-Pilla <pilla.jason@gmail.com>
2025-11-29 21:55:12 -0500

    made public fields serialized private, renamed field rb to rigidbody

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,27 +2,30 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float speed = 2f;          // horizontal speed
-    public float moveDistance = 1f;   // patrol distance from start position
+    [SerializeField] private float moveSpeed = 2f;          // horizontal speed
+    [SerializeField] private float moveDistance = 1f;   // patrol distance from start position
 
-    private Rigidbody2D rb;
+    // Components
+    private new Rigidbody2D rigidbody;
+
     private float leftBoundary;
     private float rightBoundary;
     private int direction = 1;        // 1 = right, -1 = left
 
-    void Start()
+    private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rigidbody = GetComponent<Rigidbody2D>();
 
         float startX = transform.position.x;
         leftBoundary = startX - moveDistance;
         rightBoundary = startX + moveDistance;
     }
 
-    void Update()
+    private void Update()
     {
         // Move horizontally only
-        rb.linearVelocity = new Vector2(direction * speed, 0f);
+        rigidbody.linearVelocity = new Vector2(direction * moveSpeed, 0f);
+
         // Flip sprite based on direction
         transform.localScale = new Vector3(-direction, 1, 1);
 
