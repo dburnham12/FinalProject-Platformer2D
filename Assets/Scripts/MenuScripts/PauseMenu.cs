9fdf29f89e87bef2dd171509e48755dfc829e749
J-Pilla <pilla.jason@gmail.com>
2025-11-30 19:31:15 -0500

    added condition where menu only pops up if timeScale > 0

--- Assets/Scripts/MenuScripts/PauseMenu.cs
+++ Assets/Scripts/MenuScripts/PauseMenu.cs
@@ -4,25 +4,25 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject container;
-     // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        if(Input.GetKeyUp(KeyCode.Escape))
+        if(Input.GetKeyUp(KeyCode.Escape) && Time.timeScale > 0f)
         {
             container.SetActive(true);
-            Time.timeScale = 0.0f;
+            Time.timeScale = 0f;
         }
     }
 
     public void ResumeButton()
     {
         container.SetActive(false);
-        Time.timeScale = 1.0f;
+        Time.timeScale = 1f;
     }
 
     public void MainMenuButton()
     {
-        Time.timeScale = 1.0f;
+        Time.timeScale = 1f;
         SceneManager.LoadScene("mainMenu");
     }
 }
