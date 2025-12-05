09aa48288c44c6ab5e463e77f61af184c8824a2e
J-Pilla <pilla.jason@gmail.com>
2025-12-05 14:59:17 -0500

    added methods for level selecting

--- Assets/Scripts/MenuScripts/MainMenu.cs
+++ Assets/Scripts/MenuScripts/MainMenu.cs
@@ -1,11 +1,34 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject playButton;
+    [SerializeField] private GameObject levelButtonsContainer;
+    [SerializeField] private TMP_Text menuSwapText;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+
     public void StartGame()
     {
-        SceneManager.LoadScene("MainScene");
+        SceneManager.LoadScene("level_1"); // later use playerPref to get level_#
+    }
+
+    public void LevelSelect(int level)
+    {
+        SceneManager.LoadScene($"level_{level}");
+    }
+
+    public void MenuSwap()
+    {
+        playButton.SetActive(!playButton.activeSelf);
+        levelButtonsContainer.SetActive(!levelButtonsContainer.activeSelf);
+        menuSwapText.text = playButton.activeSelf ?
+            "Level Select" : "Back";
     }
 
     public void QuitGame()
