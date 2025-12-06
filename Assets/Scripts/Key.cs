8dfc413041022921856d120c4fe5f6777804d164
dburnham12 <dylan.burnham@student.sl.on.ca>
2025-12-05 19:10:19 -0500

    Updating key and doors to be more reactive and adding UI

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -3,12 +3,21 @@ using UnityEngine;
 public class Key : MonoBehaviour
 {
     public GameObject door;
+    private KeyUI keyUI;
+
+    private void Start()
+    {
+        keyUI = FindFirstObjectByType<KeyUI>();
+        GetComponent<SpriteRenderer>().color = door.GetComponent<SpriteRenderer>().color;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if(collision.gameObject.CompareTag("Player"))
         {
-            Destroy(door);
+            keyUI.AddKey(GetComponent<SpriteRenderer>().color);
+            SoundManager.Instance.PlaySFX("KEY", 0.5f);
+            door.GetComponent<Door>().CollectKey();
             Destroy(gameObject);
         }
     }
