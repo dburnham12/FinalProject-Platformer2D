3cea0a226abe369a2aea9bb89f38209451dcc06f
J-Pilla <pilla.jason@gmail.com>
2025-12-08 12:59:32 -0500

    made it so the current level is saved with PlayerPrefs

--- Assets/Scripts/VictoryFlag.cs
+++ Assets/Scripts/VictoryFlag.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using static MainMenu;
+using static PlayerController;
 
 public class VictoryFlag : MonoBehaviour
 {
@@ -6,9 +8,18 @@ public class VictoryFlag : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.CompareTag("Player"))
         {
             SoundManager.Instance.PlaySFX("VICTORY", 1f);
+
+            if (PlayerPrefs.HasKey(LEVEL_KEY))
+            {
+                if (Level > PlayerPrefs.GetInt(LEVEL_KEY))
+                    PlayerPrefs.SetInt("level", Level);
+            }
+            else
+                PlayerPrefs.SetInt(LEVEL_KEY, Level);
+
             container.SetActive(true);
             Time.timeScale = 0.0f;
         }
